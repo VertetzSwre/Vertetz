9e7fe627670ee7512dcfa05157529f390071320f
Dorian Arena <115998419+DoriEzze@users.noreply.github.com>
2024-10-03 21:11:31 -0300

    Delete controlador directory

--- controlador/ControladorObtenerInstituciones.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../modelo/Institucion.php';
-
-$institucionModel = new Institucion();
-$instituciones = $institucionModel->getAllInstituciones();
-if (isset($_POST['res'])) {
-echo json_encode($instituciones);
-}
-?>
--- controlador/ControladorObtenerServicios.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../modelo/Servicio.php';
-
-$servicioModel = new Servicio();
-$servicios = $servicioModel->getAllServicios();
-if (isset($_POST['res'])) {
-    echo json_encode($servicios);
-}
-?>
