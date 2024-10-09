8a85f71ded3352e9588600d1b559e241fb55efdb
Dorian Arena <115998419+DoriEzze@users.noreply.github.com>
2024-10-09 16:49:25 -0300

    Delete Controller directory

--- Controller/Area/ControladorObtenerAreas.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../../Model/Classes/Area.php';
-
-$areaModel = new Area();
-$areas = $areaModel->getAllAreas();
-if (isset($_POST['res'])) {
-    echo json_encode($areas);
-}
-?>
--- Controller/Institucion/ControladorObtenerInstituciones.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../../Model/Classes/Institucion.php';
-
-$institucionModel = new Institucion();
-$instituciones = $institucionModel->getAllInstituciones();
-if (isset($_POST['res'])) {
-echo json_encode($instituciones);
-}
-?>
