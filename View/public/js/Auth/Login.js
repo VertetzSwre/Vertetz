2d07c2078f0c51184605711f5b49145369b5b2f3
Santiago Arroyo <116035901+Sarroyo7@users.noreply.github.com>
2024-10-18 17:18:11 -0300

    Add files via upload

---
+++ View/public/js/Auth/Login.js
@@ -0,0 +1,57 @@
+$(document).ready(function () {
+	//Funcion para login
+	$('#login-form').submit( function (e) {
+		if ($('#ci').val() == "" || $('#contrasena').val() == "") {
+            Swal.fire({
+                icon: 'warning',
+                title: '¡Atención!',
+                text: 'Por favor, completa todos los campos requeridos.',
+				confirmButtonText: 'Entendido',
+            });
+		} else {
+			let cedula = $('#ci').val();
+			let contrasena = $('#contrasena').val();
+	
+			$.ajax({
+				url: '../../../../Controller/Auth/AuthController.php',
+				type: 'POST',
+				data: {
+					cedula: cedula,
+					contrasena: contrasena,
+				},
+				dataType: 'json',
+				success: function (response) {
+					console.log(response);
+					if (response.estado === 'Login exitoso!') {
+						Swal.fire({
+							icon: 'success',
+							title: '¡Bienvenido!' ,
+							text: 'Has iniciado sesión correctamente',
+							showConfirmButton: false,
+                			timer: 2000,
+						}).then(() => {
+							window.location.href = '../../../../View/public/html/General/MainDashboard.html'; // Redirige en el cliente
+						});
+					} else {
+						console.log(response);
+						Swal.fire({
+							title: '¡Error!',
+							text: 'Cédula o contraseña incorrectas.',
+							icon: 'error',
+							confirmButtonText: 'Entendido',
+						});
+					}
+				},
+				error: function(jqXHR, textStatus, errorThrown) {
+					console.error('Error en la solicitud AJAX:', textStatus, errorThrown);
+					try {
+						var jsonResponse = JSON.parse(jqXHR.responseText);
+						console.error('Respuesta del servidor:', jsonResponse);
+					} catch (e) {
+						console.error('La respuesta no es un JSON válido:', jqXHR.responseText);
+					}
+				}
+			});
+		}
+	});
+});
---
+++ View/public/js/Institucion/AllInstituciones.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+	getAllInstituciones();
+	
+	function getAllInstituciones() {
+		$.ajax({
+			url: '../../../../Controller/Institucion/ControladorObtenerInstituciones.php',
+			type: 'POST',
+			data: {
+				res: 1
+			},
+			dataType: 'json',
+			success: function (response) {
+				console.log(response);
+
+				let instituciones = response;
+				let sectionInstitutionList = $('.section-institution-list');
+				sectionInstitutionList.empty(); // Limpiar contenido previo.
+	
+				instituciones.forEach(res => {
+					let sectionGetInstitutions = $(`
+						<div class="section-get-institutions">
+							<div id="institutions-item">
+								<p>Nombre: ${res.nombre}</p>
+								<p>Calle: ${res.calle}</p>
+								<p>Esquina: ${res.esquina}</p>
+								<p>Puerta: ${res.puerta}</p>
+							</div>
+						</div>
+					`);
+				/*Enviar los resultados dentro del contenedor para mostrarlos*/ 
+				sectionInstitutionList.append(sectionGetInstitutions);
+				});
+			},
+			error: function (xhr, status, error) {
+				console.error('Error en la solicitud AJAX:', status, error);
+				console.error('Respuesta del servidor:', xhr.responseText);
+			},
+		});
+	}
+});
